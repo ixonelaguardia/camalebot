b862a075768ca148fcca5af191fe359adef525c8
ixonelaguardia <ixone.laguardia@egibide.org>
2017-01-17 20:39:42 +0100

    Código
    Cálculos para la distancia en línea recta entre dos puntos

--- server.js
+++ server.js
@@ -21,33 +21,59 @@ app.use(express.static(__dirname + "/public"));
 
     Si el servidor se está ejecutando localmente:
 
-    http://localhost:3000/plazo/2017-01-31T23:59:59.000Z
+    http://localhost:3000/distancia/43.2585907//-2.9217443000000003
 
     Devuelve un objeto JSON con el formato:
-
+	
     [{"text":"Nos separan unos 15km..."}]
 */
-app.get("/distancia/:latitud//:longitud", function (req, res) {
-
+	app.get("/distancia/:latitud//:longitud", function (req, res) {
     // Obtener la latitud y la longitud que llegan en la URL
-    var latitud = new Date(req.params.latitud);
-    var longitud = new Date(req.params.longitud);
-	
-   
-
+    var latitud = parseFloat(req.params.latitud);
+    var longitud = parseFloat(req.params.longitud); 
+	  
     // Algo no ha ido bien
-    if (!fecha_entrega) {
+    if (!latitud || !longitud) {
         return res.status(400).send();
     }
+	
+	/*
+	 DISTANCIA ENTRE DOS PUNTOS - FÓRMULA DE HAVERSINE
+	 
+	https://www.genbetadev.com/cnet/como-calcular-la-distancia-entre-dos-puntos-geograficos-en-c-formula-de-haversine
+    
+	R = radio de la Tierra
+    Δlat = lat2− lat1
+    Δlong = long2− long1
+    a = sin^2(Δlat/2) + cos(lat1) · cos(lat2) · sin^2(Δlong/2)
+    c = 2 · atan2(√a, √(1−a))
+	d = R · c
+    */
+	
+	// COORDENADAS MENDIZORROTZA
+    var LONG_MENDI = -2.686193;
+    var LATI_MENDI = 42.837425;
+    // RADIO ECUATORIAL
+    var R = 6371; // KM
+	
+	// DIFERENCIAS EN RADIANES	
+	var dif_lati = toRad(latitud - LATI_MENDI);
+	var dif_long = toRad(longitud - LONG_MENDI);
 
-	// https://www.genbetadev.com/cnet/como-calcular-la-distancia-entre-dos-puntos-geograficos-en-c-formula-de-haversine
- //   var oneDay = 24 * 60 * 60 * 1000;
-   // var dias = Math.round(Math.abs((ahora.getTime() - fecha_entrega.getTime()) / (oneDay)));
-	var distancia = 33;
+	var seno_lati = Math.sin(dif_lati/2);
+    var seno_long = Math.sin(dif_long/2);
+    var cos_lat_mendi = Math.cos(LATI_MENDI);
+    var cos_latitud = Math.cos(latitud);
+	
+	var a = Math.pow(seno_lati, 2) + cos_lat_mendi * cos_latitud * Math.pow(seno_long, 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt((1-a)));
+	
+	var distancia = Math.round(R * c);
+
     // Crear el objeto con la respuesta
     var respuesta = [
         {
-            text: "Nos separan unos " + distancia + "km...",
+          text: "Nos separan unos " + distancia + "km...",
         }
     ];
 
@@ -59,3 +85,9 @@ app.get("/distancia/:latitud//:longitud", function (req, res) {
 app.listen(PORT, function () {
     console.log("Servidor Express escuchando en el puerto " + PORT + "...");
 });
+
+/* Converts numeric degrees to radians */
+// http://stackoverflow.com/questions/5260423/torad-javascript-function-throwing-error
+function toRad(Value) {
+    return Value * Math.PI / 180;
+}
